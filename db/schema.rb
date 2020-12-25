ad6001c825897da1c2b6143c9c4bbf819c4e4c9c
yosnak13 <[yosnak13@gmail.com]>
2020-12-25 11:33:24 +0900

    migrate model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_25_022845) do
+ActiveRecord::Schema.define(version: 2020_12_25_023023) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_12_25_022845) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
