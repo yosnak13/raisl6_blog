703f5eaca8b2fa1ec557d821be550f63f6b1fb32
yosnak13 <yosnak13@gmail.com>
2020-12-25 11:42:03 +0900

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+  resources :posts do
+    resources :comments, :only => [:create, :destroy]
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
